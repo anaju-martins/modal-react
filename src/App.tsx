2ae4b87d741ef7afff40acf8740acb1a5ad7b446
Ana Júlia <aanajulia505@gmail.com>
2025-03-31 20:40:40 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <div>
+      <h1>Click on the button to open the modal</h1>
+      <button className="openModalBtn">Open</button>
+     </div>
+    </>
+  )
+}
+
+export default App
